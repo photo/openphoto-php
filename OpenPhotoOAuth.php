fa8ac2a2f4d4146571b0520c0d7e1037f6310d76
Jaisen Mathai <jaisen@jmathai.com>
2011-10-09 01:37:42 -0700

    Adding a check for empty params before using to avoid a PHP notice.

--- OpenPhotoOAuth.php
+++ OpenPhotoOAuth.php
@@ -97,7 +97,8 @@ class OpenPhotoOAuth
     curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: {$request['header']}"));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    if(!empty($params))
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
     $resp = curl_exec($ch);
     curl_close($ch);
     return $resp;
